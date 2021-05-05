de48cbcb7c04f8bec75006e5eef866906b6de0c0
Sbarrett <barrettseamus@hotmail.com>
2021-05-05 19:12:58 +0100

    VueClient-Added vuex 4 with typescipt support

--- sb015.web.vue/src/views/Home.vue
+++ sb015.web.vue/src/views/Home.vue
@@ -1,3 +1,21 @@
 <template>
   <h1>Home Page</h1>
+  <div v-if="loading">Loading</div>
 </template>
+<script lang="ts">
+import { computed, defineComponent, onMounted } from "vue";
+import { useStore } from "../store";
+import { ActionTypes } from "../store/actions";
+import { MutationType } from "../store/mutations";
+export default {
+  setup() {
+    const store = useStore();
+    const loading = computed(() => store.state.loading);
+    onMounted(() => store.dispatch(ActionTypes.GetTodoItems));
+    setTimeout(() => {
+      store.commit(MutationType.SetLoading, true);
+    }, 5000);
+    return { loading };
+  },
+};
+</script>
