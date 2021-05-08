bee66dd8ccee8b054583f1d9d0a1d16d32f881e2
Sbarrett <barrettseamus@hotmail.com>
2021-05-08 16:43:12 +0100

    Add materialze and NavBar

--- sb015.web.vue/src/App.vue
+++ sb015.web.vue/src/App.vue
@@ -1,15 +1,61 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <nav class="">
+    <div class="nav-wrapper blue darken-2">
+      <router-link to="/" class="brand-logo left indigo-text text-lighten-5"
+        >SB015</router-link
+      >
+      <ul class="right">
+        <li>
+          <a class="btn waves-effect waves-light" href="#">Place Ad</a>
+        </li>
+        <li class="hide-on-med-and-down">
+          <a href="#" class="indigo-text text-lighten-5">Login</a>
+        </li>
+        <li class="hide-on-med-and-down">
+          <a href="#" class="indigo-text text-lighten-5">Sign up</a>
+        </li>
+        <li>
+          <a
+            href="#"
+            data-target="mobile-demo"
+            class="sidenav-trigger show-on-large"
+          >
+            <i class="material-icons indigo-text text-lighten-5"
+              >account_circle</i
+            >
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <ul class="sidenav sidenav-close" id="mobile-demo">
+    <li>
+      <a href="#"><i class="material-icons">description</i>My Ads</a>
+    </li>
+    <li>
+      <a href="#"><i class="material-icons">comment_blank</i>Messages</a>
+    </li>
+    <li>
+      <a href="#"><i class="material-icons">favorite</i>Saved Ads</a>
+    </li>
+    <li>
+      <a href="#"><i class="material-icons">star</i>Saved Searches</a>
+    </li>
+    <li class="divider"></li>
+    <li><router-link to="/about">About</router-link></li>
+  </ul>
   <router-view />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-
+import { defineComponent, onMounted } from "vue";
+import M from "materialize-css";
 export default defineComponent({
   name: "App",
+  setup() {
+    onMounted(() => {
+      M.AutoInit();
+    });
+  },
 });
 </script>
